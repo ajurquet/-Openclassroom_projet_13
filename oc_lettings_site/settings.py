0b2ffb3bf7f62856d1c9d615f4beeeb81ad9c3b5
Alexandre JURQUET <a.jurquet@gmail.com>
2021-12-31 10:42:21 +0100

    modif settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -14,19 +14,19 @@ env = environ.Env(
 )
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-sentry_sdk.init(
-    dsn = env('SENTRY_URL'),
-    integrations=[DjangoIntegration()],
+# sentry_sdk.init(
+#     dsn = env('SENTRY_URL'),
+#     integrations=[DjangoIntegration()],
 
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
+#     # Set traces_sample_rate to 1.0 to capture 100%
+#     # of transactions for performance monitoring.
+#     # We recommend adjusting this value in production.
+#     traces_sample_rate=1.0,
 
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
+#     # If you wish to associate users to errors (assuming you are using
+#     # django.contrib.auth) you may enable sending PII data.
+#     send_default_pii=True
+# )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/

59c5ad0ac4d698190ccf5d3db5e4efe4add49766
Alexandre JURQUET <a.jurquet@gmail.com>
2021-12-31 11:07:40 +0100

    changement config.yml

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
 
 DATABASES = {
     # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
-    # 'default': env.db(),
+    'default': env.db(),
     # read os.environ['SQLITE_URL']
     'extra': env.db('SQLITE_URL', default='sqlite:////tmp/my-tmp-sqlite.db')
 }

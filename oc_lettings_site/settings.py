a4bdf4a9d1fcb2ca3689c3d63c1809c40bf57916
Alexandre JURQUET <a.jurquet@gmail.com>
2021-12-31 10:12:17 +0100

    modif settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -5,8 +5,11 @@ import os
 import environ
 from sentry_sdk.integrations.django import DjangoIntegration
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 sentry_sdk.init(
     dsn = env('SENTRY_URL'),
@@ -21,10 +24,6 @@ sentry_sdk.init(
     # django.contrib.auth) you may enable sending PII data.
     send_default_pii=True
 )
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
